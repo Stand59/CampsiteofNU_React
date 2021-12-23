a0e4853b2d2c369dc21221259948630ec272035a
NathanStanding <standingtall1000@gmail.com>
2021-12-23 09:11:20 -0700

    Render campsite and comments

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
-
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
+import '../App.css';
 
 class CampsiteInfo extends Component{
     constructor(props) {
         super(props);
     }
-    render() {
-        if (this.props.campsite) {
-            return (<div className="row"></div>);
-        }else {
-            return(<div></div>);
-            }
+
+    renderCampsite(campsite){
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+    renderComments(comments) {
+        if(comments){
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map((comments) => 
+                    <div className="m-3">{comments.text} <br></br> -- {comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}  </div>)
+                    }    
+                </div>
+            );
         }
     }
+
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                </div>
+            );
+        }else {
+            return(
+                <div></div>
+            );
+        }
+    }
+}
 
 export default CampsiteInfo;
