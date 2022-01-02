8d1b8e4c81a503cb01500dfb39bab8e6ed0e006d
NathanStanding <standingtall1000@gmail.com>
2022-01-01 21:49:31 -0700

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Directory from './DirectoryComponents';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
 
 class Main extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
-      selectedCampsite: null
     };
   }
 
-  onCampsiteSelect(campsiteID) {
-    this.setState({selectedCampsite: campsiteID });
-}
-
-
   render() {
+    
+    const HomePage = () => {
+      return (
+        <Home />
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
-        <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+        <Switch>
+            <Route path='/home' component={HomePage} />
+            <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+            <Redirect to='/home' />
+        </Switch>
         <Footer />
       </div>
     );
