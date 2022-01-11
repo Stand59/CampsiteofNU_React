e7833f4826d4f6e2beead73979820f8e29666b25
NathanStanding <standingtall1000@gmail.com>
2022-01-11 09:04:11 -0700

    Combining Reducers

---
+++ src/redux/comments.js
@@ -0,0 +1,8 @@
+import { COMMENTS, COMMMENTS} from "../shared/comments";
+
+export const Comments= (state = COMMENTS, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
