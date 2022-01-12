3e2a251c6c3de423f602955636c2e1f4f359cb74
NathanStanding <standingtall1000@gmail.com>
2022-01-12 12:52:25 -0700

    Redux Thunk

--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap'
 import { Link } from 'react-router-dom';
 
+import { Loading } from './LoadingComponent';
+
 function RenderDirectoryItem({campsite}) {
     return (
         <Card>
@@ -16,33 +18,52 @@ function RenderDirectoryItem({campsite}) {
 }
 
 function Directory(props) {
+    const directory = props.campsites.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} />
+            </div>
+        );
+    });
 
-        const directory = props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <RenderDirectoryItem campsite={campsite} />
-                </div>
-            );
-        });
-
-        return(
+    if (props.campsites.isLoading) {
+        return (
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                            <BreadcrumbItem><Link active>Directory</Link></BreadcrumbItem>
-                        </Breadcrumb>
-                        <h2>Directory</h2>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    {directory}
+                    <Loading />
                 </div>
             </div>
         );
     }
+    if (props.campsites.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.campsites.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link active>Directory</Link></BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
+}
 
 
 
